c963897adfd2b03aea09757cf46e4a577cfb1717
Thomas Li-Smith <geekcentric@Thomass-MacBook-Pro-2.local>
2024-06-01 17:26:10 -0700

    add mongodb

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/TS22082/dat_board_server/api/test"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/dat_board")
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		fmt.Printf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		fmt.Printf("Failed to ping MongoDB: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
 	http.HandleFunc("/", test.HelloHandler)
 	http.HandleFunc("/2", test.HelloHandler2)
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		fmt.Printf("Server failed to start: %v", err)
 	}
